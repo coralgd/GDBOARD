49f1d111258f10b1f9de7a5e11ebfe6721317cb5
coralgd <fufianz@gmail.com>
2026-01-04 00:09:34 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+export const app = initializeApp({
+  apiKey: "ВАШ_API_KEY",
+  authDomain: "ВАШ_PROJECT_ID.firebaseapp.com",
+  projectId: "ВАШ_PROJECT_ID"
+});
