f55d251c3f4e2f4d71fb0dd088ffa12e0b342a91
coralgd <fufianz@gmail.com>
2026-01-04 00:15:14 +0300

    Update board.js

--- board.js
+++ board.js
@@ -1,28 +1,19 @@
-import {
-  getFirestore,
-  collection,
-  query,
-  orderBy,
-  onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-import { getAuth } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import "./firebase.js";
+import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const db = getFirestore();
-const auth = getAuth();
 const list = document.getElementById("list");
 
-const q = query(collection(db, "users"), orderBy("points", "desc"));
-
-onSnapshot(q, snap => {
+onSnapshot(collection(db, "users"), snapshot => {
   list.innerHTML = "";
-  snap.forEach(doc => {
-    const d = doc.data();
-    if (d.status !== "verified") return;
-
-    const li = document.createElement("li");
-    li.textContent = `${d.username} — ${d.points}`;
-    list.appendChild(li);
-  });
+  snapshot.docs
+    .map(d => d.data())
+    .sort((a,b) => b.points - a.points)
+    .forEach(u => {
+      if(u.status === "verified") {
+        const li = document.createElement("li");
+        li.textContent = `${u.username} — ${u.points}`;
+        list.appendChild(li);
+      }
+    });
 });
